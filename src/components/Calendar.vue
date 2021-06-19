2d02f9617ffb2ac59d70a6e16b5092afff78a47c
miyazawayamato <yamato8781@gmail.com>
2021-06-19 20:01:19 +0900

    コメント削除

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -41,8 +41,6 @@ export default {
                 todoData = response.data
             })
             
-            console.log(todoData)
-            // this.schedule = todoData
             return this.getCalendar(todoData);
         }
     },
