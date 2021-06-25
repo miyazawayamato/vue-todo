49fdfdf67f7bc3e17310ac932b9eaf36fd38226a
miyazawayamato <yamato8781@gmail.com>
2021-06-25 20:29:52 +0900

    6/26

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     publicPath: '/calendar/',
 }
+
+// vueアプリデプロイ方法(GithubPage)
+// vue.config.js作成
+// gh-pagesのインストール
+// scriptsにdeployを追加
+// build
+// distのindex.html内のsrc,herfのパスの変更
+// gitignoreからdistを削除
+// pushとdeploy
+// https://miyazawayamato.github.io/{リポジトリ名}/dist/index.html
