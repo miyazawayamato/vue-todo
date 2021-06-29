8dc05e803bdd4f5a7dbc3baf47b37bcc4a1cdb22
miyazawayamato <yamato8781@gmail.com>
2021-06-29 21:33:58 +0900

    デプロイ

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,21 @@ module.exports = {
 // vue.config.js作成
 // gh-pagesのインストール
 // scriptsにdeployを追加
+// gitignoreからdistを削除
+
 // build
 // distのindex.html内のsrc,herfのパスの変更
-// gitignoreからdistを削除
 // pushとdeploy
 // https://miyazawayamato.github.io/{リポジトリ名}/dist/index.html
+
+{/* <head><meta charset=utf-8>
+<meta name=viewport content="width=device-width,initial-scale=1">
+<title>vue-cal</title>
+<link href=./static/css/app.82f3795cbd73cf2ff5e4abcd85042b22.css rel=stylesheet>
+</head>
+<body>
+<div id=app></div>
+<script type=text/javascript src=./static/js/manifest.2ae2e69a05c33dfc65f8.js></script>
+<script type=text/javascript src=./static/js/vendor.bafa15280dc5ae2fb771.js></script>
+<script type=text/javascript src=./static/js/app.7612732cd8b65d4655e0.js></script>
+</body> */}
